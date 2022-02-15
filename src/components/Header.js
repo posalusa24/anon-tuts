3e4f5c38ba24bf9d58984c07cf83bcc640a6107d
Kaung Myat Kyaw <kaungmyatkyaw365@gmail.com>
2022-02-15 11:27:02 +0630

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,11 @@ export default function Header(){
     return (
         <header>
             <h1 class="header--title">Myo Gyi Fan Page</h1>
-            <a href="#" class="header--home"><p>Home</p></a>
-            <a href="#" class="header--about"><p>About</p></a>
+            <div class="header--nav">
+                <a href="#" class="header--home"><p>Home</p></a>
+                <a href="#" class="header--about"><p>About</p></a>
+            </div>
+           
         </header>
     )
 }
