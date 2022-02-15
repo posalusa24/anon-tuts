78dd1781d499f3372cc408da6a47de1757d86571
Kaung Myat Kyaw <kaungmyatkyaw365@gmail.com>
2022-02-15 18:37:34 +0630

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import motherland from "./images/motherland.png"
+import ggsong from "./images/ggsong.mp3"
+
 export default function App(){
   return (
     <div>
       <Header />
+      <img className="myanmarKwa--img" src={motherland}/>   
+      <div className="myanmarKwa--content">  
+        <h1 className="myanmarKwa--text">Made in Myanmar</h1>
+        <h3>Please play at least 30sec for Patriotism</h3> 
+        <audio controls>
+          <source src={ggsong} type="audio/mpeg"/>
+        </audio>
+      </div>
       <Footer />
     </div>
   )
