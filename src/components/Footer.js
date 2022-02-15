d2013d97eb459085de12c138a73057f244f7d1cf
Kaung Myat Kyaw <kaungmyatkyaw365@gmail.com>
2022-02-15 18:11:12 +0630

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,15 +7,24 @@ import githubIcon from "../images/github.png";
 export default function Footer(){
     return(
         <div className="footer">
-            <h1 className="footer--title">TUTORIAL</h1>
-            <p>We create possibilities<br/>for the connected world.</p>
-            <div className="footer--img">
-                <img src={facebookIcon}/>
-                <img src={twitterIcon}/>
-                <img src={discordIcon}/>
-                <img src={githubIcon}/>
+            <div className="footer--content">
+                <div>
+                    <h1 className="footer--title">TUTORIAL</h1>
+                    <p>We create possibilities<br/>for the connected world.</p>
+                    <div className="footer--img">
+                        <a href="https://www.facebook.com/myintmyataung173"><img className="footer--icon" src={facebookIcon}/></a>
+                        <a href="https://twitter.com/chadgelina"><img className="footer--icon" src={twitterIcon}/></a>
+                        <a href="https://discord.gg/sNRJRbEAgd"><img className="footer--icon" src={discordIcon}/></a>
+                        <a href="https://github.com/posalusa24/anon-tuts"><img className="footer--icon" src={githubIcon}/></a>
+                    </div>
+                    <p className="footer--copyright">All rights reserved 2022.</p>
+                </div>
+                <div className="footer--nav">
+                    <a href="#" className="footer--home"><p>Home</p></a>
+                    <div className="header--verticleLine"></div>
+                    <a href="#" className="footer--about"><p>About</p></a>
+                </div>
             </div>
-            <p>All rights reserved 2022.</p>
         </div>
     )
 }
