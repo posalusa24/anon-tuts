6e9169d1a363b0d3c95f4fe98d5b9754242aa189
posalusa24 <posalusa24@gmail.com>
2022-02-15 12:04:16 +0630

    Fix icons not appearing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,8 @@
-import React from "react"
+import React from "react";
+import facebookIcon from "../images/facebook.png";
+import twitterIcon from "../images/twitter.png";
+import discordIcon from "../images/discord.png";
+import githubIcon from "../images/github.png";
 
 export default function Footer(){
     return(
@@ -6,10 +10,10 @@ export default function Footer(){
             <h1 className="footer--title">TUTORIAL</h1>
             <p>We create possibilities<br/>for the connected world.</p>
             <div className="footer--img">
-                <img src="../images/facebook.png"/>
-                <img src="../images/twitter.png"/>
-                <img src="../images/discord.png"/>
-                <img src="../images/github.png"/>
+                <img src={facebookIcon}/>
+                <img src={twitterIcon}/>
+                <img src={discordIcon}/>
+                <img src={githubIcon}/>
             </div>
             <p>All rights reserved 2022.</p>
         </div>
