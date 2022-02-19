cf5ba62dfe1760c649663c1b8b5aebfabccd9958
Kaung Myat Kyaw <kaungmyatkyaw365@gmail.com>
2022-02-19 23:33:24 +0630

    commit on "main"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import motherland from "./images/motherland.png"
+
 
 export default function App(){
   return (
     <div>
       <Header />
-      <img className="myanmarKwa--img" src={motherland}/>   
-      <div className="myanmarKwa--content">  
-        <h1 className="myanmarKwa--text">Made in Myanmar</h1>
-        <h3>Please play at least 60sec for Patriotism</h3> 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/UhIpxANr2vo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
       <Footer />
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header(){
     return (
         <header>
-            <h1 className="header--title">Myo Gyi Fan Page</h1>
+            <h1 className="header--title">ANON TUTS</h1>
             <div className="header--nav">
                 <a href="#" className="header--home"><p>Home</p></a>
                 <div className="header--verticleLine"></div>
